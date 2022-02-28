4cc65f6b088c6a81bf814da261fbf78070b71cb2
nick-coury <nick.coury@sbcglobal.net>
2022-02-28 13:17:08 -0500

    Update scatterplot.js

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -53,7 +53,7 @@ const data3 = d3.csv("data/scatter.csv").then((data) => {
    .enter()  
    .append("circle") 
      .attr("class", "circle") 
-     .attr("x", (d) => xScale3(d.day)) 
+     .attr("x", (d) => xScale3(d.day) + 10) 
      .attr("y", (d) => yScale3(d.score)) 
      .attr("height", (d) => (height - margin.bottom) - yScale3(d.score)) 
      .attr("width", xScale3.bandwidth()) 
